da30a4236552f8a93990a0e55cb7558b1298f79a
Lydia Marie Barr <lydiamariebarr09@gmail.com>
2024-01-31 12:46:35 +0900

    Add Target component for hit testing

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,10 @@
 //! A blank canvas for getting started on the ODROID-N2L and ODROID-VU5A.
 
 use bevy::{
-    diagnostic::FrameTimeDiagnosticsPlugin, diagnostic::LogDiagnosticsPlugin, prelude::*,
+    diagnostic::FrameTimeDiagnosticsPlugin,
+    diagnostic::LogDiagnosticsPlugin,
+    prelude::*,
+    sprite::collide_aabb::{collide, Collision},
     sprite::MaterialMesh2dBundle,
 };
 
@@ -33,14 +36,15 @@ fn main() {
         .add_startup_system(setup)
         .insert_resource(FixedTime::new_from_secs(TIME_STEP))
         .add_system(bevy::window::close_on_esc)
-        .add_system(mouse_click_system)
+        .add_system(touch_system)
         .run();
 }
 
-fn mouse_click_system(
+fn touch_system(
     mut commands: Commands,
     mut meshes: ResMut<Assets<Mesh>>,
     mut materials: ResMut<Assets<ColorMaterial>>,
+    mut targets: Query<&Transform, With<Target>>,
     touches: Res<Touches>,
 ) {
     for touch in touches.iter_just_pressed() {
@@ -51,14 +55,32 @@ fn mouse_click_system(
         );
 
         let position = touch.position();
-        let translation = Vec3::new(position.x - 400.0, 240.0 - position.y, 0.0);
+        let translation = Vec3::new(position.x - 400.0, 240.0 - position.y, 2.0);
 
-        commands.spawn(MaterialMesh2dBundle {
-            mesh: meshes.add(shape::Circle::new(50.).into()).into(),
-            material: materials.add(ColorMaterial::from(Color::PURPLE)),
-            transform: Transform::from_translation(translation),
-            ..default()
-        });
+        for target in targets.iter() {
+            if let Some(collision) = collide(
+                target.translation,
+                target.scale.truncate(),
+                translation,
+                Vec2::splat(1.0),
+            ) {
+                let mut translation = target.translation;
+                translation.z = 2.0;
+                commands.spawn(MaterialMesh2dBundle {
+                    mesh: meshes.add(shape::Circle::new(45.).into()).into(),
+                    material: materials.add(ColorMaterial::from(Color::rgb(0.5, 0.5, 1.0))),
+                    transform: Transform::from_translation(translation),
+                    ..default()
+                });
+                translation.z = 3.0;
+                commands.spawn(MaterialMesh2dBundle {
+                    mesh: meshes.add(shape::Circle::new(30.).into()).into(),
+                    material: materials.add(ColorMaterial::from(BACKGROUND_COLOR)),
+                    transform: Transform::from_translation(translation),
+                    ..default()
+                });
+            }
+        }
     }
 }
 
@@ -69,44 +91,70 @@ fn setup(
 ) {
     commands.spawn(Camera2dBundle::default());
 
-    // Rectangle
+    // Rectangle.Vertical.Left
     commands.spawn(SpriteBundle {
         sprite: Sprite {
             color: Color::rgb(0.25, 0.25, 0.75),
-            custom_size: Some(Vec2::new(10.0, 300.0)),
+            custom_size: Some(Vec2::new(10.0, 350.0)),
             ..default()
         },
         transform: Transform::from_translation(Vec3::new(-60., 0., 0.)),
         ..default()
     });
-    // Rectangle
+    // Rectangle.Vertical.Right
     commands.spawn(SpriteBundle {
         sprite: Sprite {
             color: Color::rgb(0.25, 0.25, 0.75),
-            custom_size: Some(Vec2::new(10.0, 300.0)),
+            custom_size: Some(Vec2::new(10.0, 350.0)),
             ..default()
         },
         transform: Transform::from_translation(Vec3::new(60., 0., 0.)),
         ..default()
     });
-    // Rectangle
+    // Rectangle.Horizontal.Top
     commands.spawn(SpriteBundle {
         sprite: Sprite {
             color: Color::rgb(0.25, 0.25, 0.75),
-            custom_size: Some(Vec2::new(300.0, 10.0)),
+            custom_size: Some(Vec2::new(350.0, 10.0)),
             ..default()
         },
         transform: Transform::from_translation(Vec3::new(0., 60., 0.)),
         ..default()
     });
-
+    // Rectangle.Horizontal.Bottom
     commands.spawn(SpriteBundle {
         sprite: Sprite {
             color: Color::rgb(0.25, 0.25, 0.75),
-            custom_size: Some(Vec2::new(300.0, 10.0)),
+            custom_size: Some(Vec2::new(350.0, 10.0)),
             ..default()
         },
         transform: Transform::from_translation(Vec3::new(0., -60., 0.)),
         ..default()
     });
+    let mut index = 0;
+    for y in (-120..=120).step_by(120) {
+        for x in (-120..=120).step_by(120) {
+            commands.spawn((target(Vec3::new(x as f32, y as f32, 0.)), Target(index)));
+            index += 1;
+        }
+    }
+}
+
+#[derive(Component)]
+struct Target(u8);
+fn target(translation: Vec3) -> SpriteBundle {
+    let scale = Vec3::new(110.0, 110.0, 1.0);
+
+    SpriteBundle {
+        sprite: Sprite {
+            color: BACKGROUND_COLOR,
+            ..default()
+        },
+        transform: Transform {
+            translation,
+            scale,
+            ..default()
+        },
+        ..default()
+    }
 }
